c3219cb6f1b3bbef64627f11e2737f1512e0f918
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-14 17:41:16 +0530

    [Akshay]ADDED: path to position property demo page

--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@ document.querySelector('#nav-bar').innerHTML = `
             <ul class="options">
                 <li><a href="../pages/flex_box.html">Flex Box</a></li>
                 <li><a href="../pages/grid.html">Grid</a></li>
-                <li><a href="">Positions</a></li>
+                <li><a href="../pages/position.html">Positions</a></li>
                 <li><a href="">###</a></li>
                 <li><a href="">###</a></li>
             </ul>
