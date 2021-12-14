0fcd68291c86d45fde6580051e285df23a17920f
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-14 10:15:11 +0530

    [Akshay]ADDED: common navbar in a js file

---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.querySelector('#nav-bar').innerHTML = `
+    <div class="nav-bar">
+        <nav class="nav">
+            <ul class="options">
+                <li><a href="../pages/flex_box.html">Flex Box</a></li>
+                <li><a href="">Grid</a></li>
+                <li><a href="">Positions</a></li>
+                <li><a href="">###</a></li>
+                <li><a href="">###</a></li>
+            </ul>
+        </nav>
+    </div>
+`;
